d407fdf81f2aae1393650fcaefbe30e126b5af10
LuiKi <larik20041998@mail.ru>
2023-12-16 03:07:09 +0300

    create a new HelloWorld

--- Example/src/main/java/HelloWorld.java
+++ Example/src/main/java/HelloWorld.java
@@ -1,5 +1,16 @@
+import cars.*;
+
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Truck truck = new Truck();
+        truck.setSpeed(5);
+        Ship ship = new Ship();
+        ship.setSpeed(4);
+        List<Car> cars = List.of(truck,ship);
+        for (Car car:cars) {
+            car.move();
+        }
     }
 }
